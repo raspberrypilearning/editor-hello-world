a2e45fae717e9923ab65ac6bc443bde26ec29eee
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-04-07 11:49:25 +0100

    New translations main.py (Dutch)

--- nl-NL/code/complete/main.py
+++ nl-NL/code/complete/main.py
@@ -2,24 +2,24 @@
 from datetime import datetime
 from random import randint
 
-world = '🌍🌎🌏'
+wereld = '🌍🌎🌏'
 python = 'Python 🐍'
-fire = '🔥'
+vuur = '🔥'
 
-# Function definitions        
-def roll_dice():
+# Functiedefinities        
+def gooi_dobbelsteen():
     max = input('How many sides on your dice?:')
     print(f'That is a D {max}')
     roll = randint(1, int(max))
     print(f'You rolled a {roll} {fire * roll}')
     
-# Put code to run under here
+# Zet de code om uit te voeren hieronder
 print(f'Hello {world}')
 print(f'Welcome to {python}')
 print(f'{python} is good at maths!')
 print(f'{111111111 * 111111111}')
 print(f'The date and time is {datetime.now()}')
-roll_dice()  # Call the roll dice function
+gooi_dobbelsteen() #Roep de dobbelsteen functie aan
 print(f'I ❤️ ...')   
-print(f'... makes me 😃')   
+print(f'... maakt mij 😃')   
 print(f'I would like to make ... with {python}')
