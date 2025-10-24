2ab0eff17cd329e51d728690fea5b335b965d58f
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-10-24 15:58:52 +0100

    New translations main.py (French)

---
+++ fr-FR/code/complete/main.py
@@ -0,0 +1,25 @@
+# imports
+from datetime import datetime
+from random import randint
+
+monde = '🌍🌎🌏'
+python = 'Python 🐍'
+feu = '🔥'
+
+# Définitions de fonctions        
+def roule_de():
+    max = input('How many sides on your dice?:')
+    print(f'That is a D {max}')
+    roule = randint(1, int(max))
+    print(f'You rolled a {roll} {fire * roll}')
+    
+# Mettre le code à exécuter ci-dessous
+print(f'Hello {world}')
+print(f'Welcome to {python}')
+print(f'{python} is good at maths!')
+print(f'{111111111 * 111111111}')
+print(f'The date and time is {datetime.now()}')
+roule_de() # Appel la fonction lancer de dés
+print(f'I ❤️ ...')   
+print(f'... me rend 😃')   
+print(f'I would like to make ... avec {python}')
