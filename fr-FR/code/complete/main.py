ed9b35c465a20f7647a2ac026389177f15ac7861
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-10-30 17:00:09 +0000

    New translations main.py (French)

--- fr-FR/code/complete/main.py
+++ fr-FR/code/complete/main.py
@@ -9,7 +9,7 @@ feu = '🔥'
 # Définitions de fonctions        
 def roule_de():
     max = input('Combien de faces y a-t-il sur ton dé ?')
-    print(f"C'est un D {max}")
+    print(f'C\'est un D {max}')
     roule = randint(1, int(max))
     print(f'Tu as obtenu un {roule} {feu * roule}')
     
@@ -20,6 +20,6 @@ print(f'{python} est bon en maths !')
 print(f'{111111111 * 111111111}')
 print(f'La date et l'heure sont {datetime.now()}')
 roule_de() # Appel la fonction lancer de dés
-print(f"J'❤️ ...")   
+print(f'J\'❤️ ...')   
 print(f'... me rend 😃')   
 print(f'Je voudrais faire ... avec {python}')
